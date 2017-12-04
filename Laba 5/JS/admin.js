0d00ecfb1427b58c9e7e6b995fe90282a3420b11
Predator12 <panchushak02@ukr.net>
2017-12-04 22:33:52 +0200

    Laba 9

--- Laba 5/JS/admin.js
+++ Laba 5/JS/admin.js
@@ -1,31 +1,41 @@
 var i = 0;
 
- function isOnline() {
-     return window.navigator.onLine;
- }
+function isOnline() {
+    return window.navigator.onLine;
+}
 
- function AddNews() {
-     if (($('#longdescription').val()==="") || ($('#shortdescription').val()==="") || ($('#namearticle').val()==="")) {
-         alert('Заповніть всі дані');
-         return false;
-     }
+function AddNews() {
+    if (($('#longdescription').val()==="") || ($('#shortdescription').val()==="") || ($('#namearticle').val()==="")) {
+        alert('Заповніть всі дані');
+        return false;
+    }
 
-     if (isOnline()) {
-         $('#longdescription').val('');
-         $('#shortdescription').val('');
-         $('#namearticle').val('');
-         alert('Новину успішно додано');
-     }
-
+    if (isOnline()) {
+        alert('Новину успішно додано');
+    }
     else {
-          i++;
-         var list = [];
-         list.push({"name":$('#namearticle').val(),
-             "shortdescription":$('#shortdescription').val(),
-            "longdescription":$('#longdescription').val()});
-         localStorage.setItem(i, JSON.stringify(list));
-         $('#longdescription').val('');
-         $('#shortdescription').val('');
-         $('#namearticle').val('');
-     }
- }
+        if (useLocalStorage){
+            i++;
+            var list = [];
+            list.push({
+                "name": $('#namearticle').val(),
+                "shortdescription": $('#shortdescription').val(),
+                "longdescription": $('#longdescription').val()
+            });
+            localStorage.setItem(i, JSON.stringify(list));
+        }
+        else {
+            var transaction = db.transaction(["news"], "readwrite");
+            var store = transaction.objectStore("news");
+            var news1 = {
+                name: $('#namearticle').val(),
+                shortdescription: $('#shortdescription').val(),
+                longdescription: $('#longdescription').val()
+            };
+            store.add(news1);
+        }
+    }
+    $('#longdescription').val('');
+    $('#shortdescription').val('');
+    $('#namearticle').val('');
+}
